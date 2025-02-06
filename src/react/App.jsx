733d0cfc5c972c7d82640bbbee4d38047e4e5048
MouhannadK <118668719+MouhannadK@users.noreply.github.com>
2025-02-06 21:52:01 +0100

    Update App.jsx
    farbe geandert

--- src/react/App.jsx
+++ src/react/App.jsx
@@ -17,12 +17,12 @@ const theme = createTheme({
     ].join(',')
   },
   palette: {
-    primary: { main: '#05F782', gradient: 'linear-gradient(to bottom, #05F782, #06C96B)' },
+    primary: { main: '#42A5F5', gradient: 'linear-gradient(to bottom, #05F782, #06C96B)' },
     secondary: { main: '#EA4A4A' },
     scenidBlue: { main: '#3200BE', gradient: 'linear-gradient(to bottom, #3200BE, #280097)', gradientHorizontal: 'linear-gradient(to left, #3200BE, #280097)' },
     scenidDarkBlue: { main: '#280097' },
     background: {
-      default: '#05F782',
+      default: '#FFFFFF',
       primary: '#05F782'
     },
     text: {
